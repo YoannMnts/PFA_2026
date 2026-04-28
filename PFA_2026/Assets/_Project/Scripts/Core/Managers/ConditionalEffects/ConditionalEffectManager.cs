f74eaf1c96515fcd28f27d94694062ea6cd00b26
YoannMnts <yoann.montesinos@gmail.com>
2026-04-28 22:02:41 +0200

    new conditionalEffect Datas
    now need to rework how to compute it

--- PFA_2026/Assets/_Project/Scripts/Core/Managers/ConditionalEffects/ConditionalEffectManager.cs
+++ PFA_2026/Assets/_Project/Scripts/Core/Managers/ConditionalEffects/ConditionalEffectManager.cs
@@ -1,6 +1,7 @@
 ﻿using Naussilus.Core.Conditions;
 using Naussilus.Core.Consequences;
 using Naussilus.Core.Managements.RoomDatas.ActionDatas.Categorys;
+using Naussilus.Core.Managers.Npcs;
 using Naussilus.Core.NpcDatas;
 using UnityEngine;
 
@@ -8,31 +9,38 @@ namespace Naussilus.Core.Managers
 {
     public static class ConditionalEffectManager
     {
+        public static NpcData[] currentNpcsData { get; private set; }
         public static void ComputeConditionalEffect(this ConditionalEffect conditionalEffect, NpcData currentNpcData, Category[] currentCategories)
         {
+            currentNpcsData = conditionalEffect.IsEnumeration
+                ? NpcManager.GetSelectedNpcs(conditionalEffect.CurrentNpcTarget, currentNpcData)
+                : new[] { currentNpcData };
+            
             Condition[] currentConditions = conditionalEffect.Conditions;
             Consequence[] currentConsequences = conditionalEffect.Consequences;
-
-            currentConditions.ComputeAllCondition(currentNpcData, currentCategories, out var validNpcs);
-            Debug.Log($"[ConditionalEffectManager] Compute Conditions");
-            for (int i = 0; i < validNpcs.Count; i++)
+            
+            for (int i = 0; i < currentNpcsData.Length; i++)
             {
-                currentConsequences.ComputeAllConsequence(validNpcs[i], currentCategories);
-                
+                currentConditions.ComputeAllCondition(currentNpcData, currentCategories, out var validNpcs);
+                for (int j = 0; j < validNpcs.Count; j++)
+                    currentConsequences.ComputeAllConsequence(validNpcs[i], currentCategories);
             }
         }
         
         public static void ComputeConditionalEffect(this ConditionalEffect conditionalEffect, NpcData currentNpcData)
         {
+            currentNpcsData = conditionalEffect.IsEnumeration
+                ? NpcManager.GetSelectedNpcs(conditionalEffect.CurrentNpcTarget, currentNpcData)
+                : new[] { currentNpcData };
+            
             Condition[] currentConditions = conditionalEffect.Conditions;
             Consequence[] currentConsequences = conditionalEffect.Consequences;
 
-            currentConditions.ComputeAllCondition(currentNpcData, out var validNpcs);
-            Debug.Log($"[ConditionalEffectManager] Compute Conditions");
-            for (int i = 0; i < validNpcs.Count; i++)
+            for (int i = 0; i < currentNpcsData.Length; i++)
             {
-                currentConsequences.ComputeAllConsequence(validNpcs[i]);
-                Debug.Log($"[ConditionalEffectManager] Compute Consequences");
+                currentConditions.ComputeAllCondition(currentNpcData, out var validNpcs);
+                for (int j = 0; j < validNpcs.Count; j++)
+                    currentConsequences.ComputeAllConsequence(validNpcs[i]);
             }
         }
     }
