d0c7b99ce64054e6b5b99ad9d9292d3e782c6ca6
YoannMnts <yoann.montesinos@gmail.com>
2026-05-03 13:42:53 +0200

    ScheduledEffect system

--- PFA_2026/Assets/_Project/Scripts/Core/Runtimes/Managements/Effects/ActionEffect.cs
+++ PFA_2026/Assets/_Project/Scripts/Core/Runtimes/Managements/Effects/ActionEffect.cs
@@ -6,26 +6,27 @@ using UnityEngine;
 
 namespace Naussilus.Core
 {
-    public struct ActionEffect
+    public class ActionEffect
     {
         public int CategoryIndex { get; private set; }
+
+        [CanBeNull] public Npc Npc { get; private set; }
         
-        [CanBeNull] public Npc NpcData { get; private set; }
-        
+        public Category[] CurrentCategories { get; private set; }
         [CanBeNull] public ConditionalEffect[] Effects { get; private set; }
         
         public Vector3 Position { get; private set; }
         
         public Expression? ActionSprite { get; private set; }
 
-        public ActionEffect(ActionEffectData data)
+        public ActionEffect(ActionEffectData data, Category[] categories)
         {
             CategoryIndex = data.CategoryIndex;
-            NpcData = NpcManager.TryGetNpc(data.NpcData?.GUID);
+            Npc = NpcManager.TryGetNpc(data.NpcData?.GUID);
+            CurrentCategories = categories;
             Effects = data.Effects?.Select(e => new ConditionalEffect(e)).ToArray();
             Position = data.Position;
             ActionSprite = data.ActionSprite != null ? new Expression(data.ActionSprite) : null;
-            
         }
     }
 }
