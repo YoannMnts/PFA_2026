9885449b39fcb1d23ad9c65edbc23131e93c31b2
YoannMnts <yoann.montesinos@gmail.com>
2026-05-15 12:46:13 +0200

    Main Menu

---
+++ PFA_2026/Assets/_Project/Scripts/Gameplay/MainMenus/MainMenuManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Naussilus.Gameplay
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        public void Play(int playerNumber)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
