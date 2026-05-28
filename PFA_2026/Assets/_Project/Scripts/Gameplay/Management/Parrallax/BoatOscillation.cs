6e7d63d1d20dff1ee555063b1c356bb4b5bb0fc2
Naël <naeldupre21@gmail.com>
2026-05-28 22:37:10 +0200

    Boat oscillation test

---
+++ PFA_2026/Assets/_Project/Scripts/Gameplay/Management/Parrallax/BoatOscillation.cs
@@ -0,0 +1,52 @@
+using System;
+using Helteix.Tools.Phases.Listeners;
+using UnityEngine;
+
+namespace Naussilus.Gameplay.Parrallax
+{
+    public class BoatOscillation : MonoPhaseListener<ManagementPhase>
+    {
+        [SerializeField] private GameObject ship;
+        [SerializeField] private AnimationCurve boatOscillationCurve;
+        [SerializeField] private float oscillationSpeed;
+        [SerializeField] private float oscillationInstensity;
+        private float currentCurveProgress;
+        private bool isOscillating = true;
+        private Vector3 basePosition;
+
+        private void Awake()
+        {
+            basePosition = ship.transform.position;
+        }
+
+        protected override void OnPhaseBegin(ManagementPhase phase)
+        {
+            base.OnPhaseBegin(phase);
+            isOscillating = true;
+        }
+
+        protected override void OnPhaseEnd(ManagementPhase phase)
+        {
+            base.OnPhaseEnd(phase);
+            isOscillating = false;
+        }
+
+        private void Update()
+        {
+            if (isOscillating)
+            {
+                Oscillate();
+            }
+        }
+
+        private void Oscillate()
+        {
+            currentCurveProgress += oscillationSpeed*Time.deltaTime;
+            if (currentCurveProgress > 1)
+            {
+                currentCurveProgress = 0;
+            }
+            ship.transform.position = basePosition + Vector3.up * (boatOscillationCurve.Evaluate(currentCurveProgress) * oscillationInstensity);
+        }
+    }
+}
